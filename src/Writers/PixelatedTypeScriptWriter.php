8ffac3e62ccc5ef756320928b75b8a01b49ead35
Ian Heggaton <ian@pixelated.com.au>
2024-04-09 12:39:08 +1000

    Refactor PixelatedTypeScriptWriter class inheritance
    Simplified the PixelatedTypeScriptWriter class through inheritance. Instead of extending from ModuleWriter and implementing a manual iterator and type export string function, the class now extends TypeDefinitionWriter. This represents a reduction in redundancies and improves maintainability.

--- src/Writers/PixelatedTypeScriptWriter.php
+++ src/Writers/PixelatedTypeScriptWriter.php
@@ -2,42 +2,15 @@
 
 namespace Pixelated\TypeScriptTransformer\Writers;
 
-use Pixelated\TypeScriptTransformer\Contracts\Constants;
-use Spatie\Enum\Enum;
-use Spatie\TypeScriptTransformer\Structures\TransformedType;
 use Spatie\TypeScriptTransformer\Structures\TypesCollection;
-use Spatie\TypeScriptTransformer\Writers\ModuleWriter;
+use Spatie\TypeScriptTransformer\Writers\TypeDefinitionWriter;
 
-class PixelatedTypeScriptWriter extends ModuleWriter
+class PixelatedTypeScriptWriter extends TypeDefinitionWriter
 {
     public function format(TypesCollection $collection): string
     {
         $output = "// noinspection TypeScriptUnresolvedReference" . PHP_EOL;
         $output .= "// @ts-nocheck" . PHP_EOL;
-
-        $iterator = $collection->getIterator();
-
-        $iterator->uasort(function (TransformedType $a, TransformedType $b) {
-            return strcmp($a->name, $b->name);
-        });
-
-        foreach ($iterator as $type) {
-            if ($type->isInline) {
-                continue;
-            }
-            $output .= $this->getTypeExportString($type);
-        }
-
-        return $output;
-    }
-
-    private function getTypeExportString(TransformedType $type): string
-    {
-        if ($type->reflection->isSubclassOf(Enum::class)
-            || in_array(Constants::class, $type->reflection->getInterfaceNames(), true)) {
-            return "export enum $type->name $type->transformed" . PHP_EOL;
-        }
-
-        return "export type $type->name = $type->transformed;" . PHP_EOL;
+        return $output . parent::format($collection);
     }
 }
