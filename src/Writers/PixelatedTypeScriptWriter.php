c4f46b62f9b347c0572a1d985e1be9775128516a
Ian Heggaton <ian@pixelated.com.au>
2024-04-09 12:49:04 +1000

    Revert "Refactor PixelatedTypeScriptWriter class inheritance"
    This reverts commit 8ffac3e62ccc5ef756320928b75b8a01b49ead35.

--- src/Writers/PixelatedTypeScriptWriter.php
+++ src/Writers/PixelatedTypeScriptWriter.php
@@ -2,15 +2,42 @@
 
 namespace Pixelated\TypeScriptTransformer\Writers;
 
+use Pixelated\TypeScriptTransformer\Contracts\Constants;
+use Spatie\Enum\Enum;
+use Spatie\TypeScriptTransformer\Structures\TransformedType;
 use Spatie\TypeScriptTransformer\Structures\TypesCollection;
-use Spatie\TypeScriptTransformer\Writers\TypeDefinitionWriter;
+use Spatie\TypeScriptTransformer\Writers\ModuleWriter;
 
-class PixelatedTypeScriptWriter extends TypeDefinitionWriter
+class PixelatedTypeScriptWriter extends ModuleWriter
 {
     public function format(TypesCollection $collection): string
     {
         $output = "// noinspection TypeScriptUnresolvedReference" . PHP_EOL;
         $output .= "// @ts-nocheck" . PHP_EOL;
-        return $output . parent::format($collection);
+
+        $iterator = $collection->getIterator();
+
+        $iterator->uasort(function (TransformedType $a, TransformedType $b) {
+            return strcmp($a->name, $b->name);
+        });
+
+        foreach ($iterator as $type) {
+            if ($type->isInline) {
+                continue;
+            }
+            $output .= $this->getTypeExportString($type);
+        }
+
+        return $output;
+    }
+
+    private function getTypeExportString(TransformedType $type): string
+    {
+        if ($type->reflection->isSubclassOf(Enum::class)
+            || in_array(Constants::class, $type->reflection->getInterfaceNames(), true)) {
+            return "export enum $type->name $type->transformed" . PHP_EOL;
+        }
+
+        return "export type $type->name = $type->transformed;" . PHP_EOL;
     }
 }
